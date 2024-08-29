a57b89bff3e31d5422648a74da83f633988eb03d
IvyRoom <contato@ivyroom.com.br>
2024-08-29 17:32:28 -0300

    v3

---
+++ js/main.1.js
@@ -0,0 +1,27 @@
+////////////////////////////////////////////////////////////////////////////////////////
+// Botão "Entrar": Envia informações de login ao backend.
+////////////////////////////////////////////////////////////////////////////////////////
+
+// Declara as variáveis necessárias.
+var FormulárioLogin = document.getElementById('Formulário-Login')
+var EmailUsuário = document.getElementById('E-mail');
+var SenhaUsuário = document.getElementById('Senha');
+
+FormulárioLogin.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    fetch('https://plataforma-backend-v2-gra0a3fnfwejcbd5.brazilsouth-01.azurewebsites.net/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email: EmailUsuário.value, senha: SenhaUsuário.value })
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error('Error:', error));
+
+});
+
+
+
