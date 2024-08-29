044080ec85082c3221526560de91fe1a649c6bbf
IvyRoom <contato@ivyroom.com.br>
2024-08-29 11:03:54 -0300

    v

--- js/main.1.js
+++ js/main.1.js
@@ -4,10 +4,23 @@
 
 // Declara as variáveis necessárias.
 var FormulárioLogin = document.getElementById('Formulário-Login')
+var EmailUsuário = document.getElementById('E-mail').value;
+var SenhaUsuário = document.getElementById('Senha').value;
 
 FormulárioLogin.addEventListener('submit', function(event) {
     event.preventDefault();
-    alert('Formulário enviado com sucesso!');
+
+    fetch('plataforma-backend-evamawanhpe4ccaw.eastus-01.azurewebsites.net/login', { // Replace with your backend URL
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email: email, senha: senha })
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error('Error:', error));
+
 });
 
 
